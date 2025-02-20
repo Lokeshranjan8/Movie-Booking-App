bed0f22bb36abce94a6fe71f70be052200856278
Lokeshranjan8 <lokesh.ranjan.phe23@itbhu.ac.in>
2025-02-21 01:54:28 +0530

    basic layout

--- server/cmd/movie-backend/main.go
+++ server/cmd/movie-backend/main.go
@@ -1,34 +1,43 @@
+//go run cmd/movie-backend/main.go  go to cmd/movie-backend the run---->> air
+//$(go env GOPATH)/bin/air -c .air.toml
+//sudo mv $(go env GOPATH)/bin/air /usr/local/bin/
+
 package main
 
 import (
-	"catgpt-backend/pkg/handler"
-	"catgpt-backend/pkg/middleware"
-	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
+	"fmt"
 	"log"
-	"net/http"
 	"os"
+	"github.com/joho/godotenv"
+	"http"
 )
 
 func main() {
 	err := godotenv.Load()
+	log.Print("hello")
 	if err != nil {
 		log.Println(err)
-		log.Fatal("Error loading .env file")
+		log.Fatal("error loading file")
 	}
 
-	router := mux.NewRouter()
-	router.Use(middleware.LoggingMiddleware)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s ssl=disable",
+		os.Getenv("HOST"), os.Getenv("USER"), os.Getenv("PASSWORD"), os.Getenv("DBNAME"), os.Getenv("PORT"),
+	)
 
-	router.HandleFunc("/chat", handler.ChatHandler).Methods("POST", "OPTIONS")
+	log.Println("Connected to database successfully")
+	log.Printf("starting server at port %s", os.Getenv("PORT"))
+	log.Println("ssss",dsn)
+	log.Fatal(http.ListenAndServe(":" + os.Getenv("PORT"), r))
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-
-	log.Printf("Starting server on port %s", port)
-	if err := http.ListenAndServe(":"+port, router); err != nil {
-		log.Fatalf("Could not start server: %s\n", err)
-	}
 }
+
+// func main() {
+// 	router := mux.NewRouter()
+// 	router.Use(middleware.LoggingMiddleware)
+
+// 	router.HandleFunc("/chat", handler.ChatHandler).Methods("POST", "OPTIONS")
+// 	log.Printf("Starting server on port %s", port)
+// 	if err := http.ListenAndServe(":"+port, router); err != nil {
+// 		log.Fatalf("Could not start server: %s\n", err)
+// 	}
+// }
