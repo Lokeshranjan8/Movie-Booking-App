e36b5ee174f993a83ee1fb473628e538afd7c107
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2025-02-23 16:07:49 +0530

    lattitude/longitude

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,46 @@
 import logo from "./assets/logo.jpeg"
+import { useState, useEffect } from "react"
+
+interface Location{
+  Lat: Number,
+  Long: Number,
+}
 
 const App = () => {
+
+  const [location, setLocation] = useState<Location | null>(null)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            Lat: position.coords.latitude,
+            Long: position.coords.longitude,
+          });
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+    }
+  });
+  
+
   return (
     <>
       <header className="w-screen flex items-center justify-around gap-2 p-2 mt-2">
         <div className="bg-red-300 h-10 flex justify-center items-center"><img className="md:w-60 w-7xl" src={logo} alt="gg" /></div>
         <input className="bg-gray-50 w-198 h-9 p-2 rounded-sm border-1 border-gray-600 shadow shadow-gray-400" type="text" placeholder="Search for Movies" />
-        <select className="bg-red-400 w-36 h-8 rounded-sm" name="Location" id="loc">
+        {/* <select className="bg-red-400 w-36 h-8 rounded-sm" name="Location" id="loc">
           <option value="Choose">Choose Location</option>
           <option value="Mumbai">Mumbai</option>
           <option value="Delhi">Delhi</option>
           <option value="Varanasi">Varanasi</option>
           <option value="London">London</option>
           <option value="Detect">Detect from GPS</option>
-        </select>        
+        </select>         */}
+        <button className="bg-red-400 w-36 h-8 rounded-sm">{location && location.Lat}</button>
         <button className="bg-red-400 w-28 h-8 rounded-sm">signIn</button>
       </header>
     </>
